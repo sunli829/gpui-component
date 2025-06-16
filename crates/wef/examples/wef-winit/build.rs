e8228976774f987d731bfa209e920f9c6a67f444
Sunli <scott_s829@163.com>
2025-06-16 19:18:18 +0800

    .

--- crates/wef/examples/wef-winit/build.rs
+++ crates/wef/examples/wef-winit/build.rs
@@ -4,5 +4,9 @@ fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
         embed_manifest(new_manifest("Wef.Example.Winit")).expect("unable to embed manifest file");
     }
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+
     println!("cargo:rerun-if-changed=build.rs");
 }
